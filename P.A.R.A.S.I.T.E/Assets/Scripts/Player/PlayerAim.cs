f3509ac253a0fdbd28bcb33fe5146e2d49a5c0ab
Aidan Sim <aidansim@colostate.edu>
2025-02-25 16:53:07 -0700

    Player View is not displayed via LineRenderer, first step for Weapons

--- P.A.R.A.S.I.T.E/Assets/Scripts/Player/PlayerAim.cs
+++ P.A.R.A.S.I.T.E/Assets/Scripts/Player/PlayerAim.cs
@@ -7,13 +7,19 @@ public class PlayerAim : MonoBehaviour
 
     [SerializeField] public LayerMask groundLayer;
 
+    private GameObject lineRendererObject;
     private Transform characterTransform;
     private Camera mainCamera;
+    private LineRenderer lineRenderer;
 
     void Awake()
     {
         mainCamera=  Camera.main;
         characterTransform = GetComponent<Transform>();
+        lineRendererObject = GameObject.Find("Line Renderer");
+        lineRenderer = lineRendererObject.GetComponent<LineRenderer>();
+
+        lineRenderer.positionCount = 2;
     }
 
     // Update is called once per frame
@@ -43,10 +49,19 @@ public class PlayerAim : MonoBehaviour
         Vector3 direction = position - characterTransform.position;
 
         direction.y = 0;
+
         if(direction.sqrMagnitude != 0.0f)
         {
             characterTransform.forward = direction;
         }
+
+        Vector3 lineRenderGoal = position;
+        lineRenderGoal.y += 1.8f;
+        Vector3 lineRenderStart = characterTransform.position;
+        lineRenderStart.y += 1.8f;
+
+        lineRenderer.SetPosition(1, lineRenderGoal);
+        lineRenderer.SetPosition(0, lineRenderStart);
     }
 
     public void LookTo(Vector3 goal)
